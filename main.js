50afcc9f8df47f6a24bb906efcca292c7ebdb861
ZanaSiaka <zanacoul21@gmail.com>
2024-03-30 03:12:46 +0000

    3h

--- main.js
+++ main.js
@@ -1,3 +1,40 @@
 // Encapsulation, Abstraction, Heritage, Polymorphisme
 
-a = "Nous commençons ce soir le poo du js"
+let employee = {
+    firstName: 'Zana',
+    lastName: 'Coulibaly',
+    pays: 'Côte d\'Ivoire',
+    direHello: function(){
+        return `${this.firstName} te dit Hello`
+    }
+}
+
+console.log(employee)
+
+// console.log(employee.direHello())
+
+// console.log(employee.pays)
+
+// console.log(employee['pays'])
+// acc = 'pays'
+
+// console.log(employee[acc])
+
+
+// let employee = new Object()
+// employee.firstName = 'Odi'
+// employee.lastName = 'N\'dja'
+
+
+// employee.age = 18
+
+
+// console.log(employee)
+
+// employee['email'] = 'zanacou210@gmail.com'
+// console.log(employee)
+
+
+// delete employee.age
+// delete employee ['email']
+
